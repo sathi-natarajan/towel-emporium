7605dd0bc6d64fe37db7b5e814c2706bc19281e2
Sathi <Sathi_natarajan@yahoo.com>
2018-09-10 18:08:41 -0700

    Towel Emporium - Version 1

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/flights', 'FlightsController@add');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
