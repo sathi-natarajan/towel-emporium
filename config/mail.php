42c0367879b94716917e8fd534b7c1b3b1809b86
Sathi <Sathi_natarajan@yahoo.com>
2018-09-17 10:39:32 -0700

    Towel Emporium - Version 1

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'towel.emporium.dev@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'sn8_2000@hotmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Towel Emporium'),
     ],
 
